eda1e89ab18585ee41af4cb10324f4865d5028f3
Kevin Zhang <kzhang@ivani.com>
2017-01-22 22:54:17 -0500

    finished HW1

--- day01/src/MyArrayList.java
+++ day01/src/MyArrayList.java
@@ -3,33 +3,70 @@ public class MyArrayList {
 	private int size;
 
 	public MyArrayList() {
-		// TODO
+		elems = new Cow[10];
+		size = 0;
 	}
 
 	public MyArrayList(int capacity) {
-		// TODO
+		elems = new Cow[capacity];
+		size = 0;
 	}
 
 	public void add(Cow c) {
-		// TODO
+		if(size == elems.length){
+		    resize("increase");
+
+        }
+        elems[size] = c;
+		size ++;
 	}
 
 	public int size() {
-		// TODO
-		return -1;
+		return size;
 	}
 
 	public Cow get(int index) {
-		// TODO
-		return null;
+		if(index < 0 || index >= size) {
+		    throw new IndexOutOfBoundsException();
+        }
+		return elems[index];
 	}
 
 	public Cow remove(int index) {
-		// TODO
-		return null;
+		Cow removed = get(index);
+
+		System.arraycopy(elems, index+1, elems, index, size-index-1);
+        size --;
+		if(size*4 <= elems.length){
+			resize("decrease");
+		}
+		return removed;
 	}
 
 	public void add(int index, Cow c) {
-		// TODO
+		get(index); //checks for index out of bounds
+		if(size == elems.length){
+		    resize("increase");
+        }
+
+		System.arraycopy(elems, index, elems, index+1, size-index);
+        elems[index] = c;
+        size ++;
+
 	}
+
+	private void resize(String direction){
+		Cow[] temp;
+
+		if(direction == "increase") {
+			temp = new Cow[elems.length*2];
+			System.arraycopy(elems, 0, temp, 0,  size);
+		}
+        else{
+			temp = new Cow[elems.length/2];
+			System.arraycopy(elems, 0, temp, 0,  size);
+		}
+
+        elems = temp;
+    }
 }
